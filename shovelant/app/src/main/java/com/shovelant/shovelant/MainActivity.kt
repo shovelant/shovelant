5f91ec3a8de5e4c33a941d7a859880ad23fcb79f
Gabriel Witkop <gwit0513@gmail.com>
2022-02-06 14:26:25 -0500

    Implement Material3 button, comment some stuff

--- shovelant/app/src/main/java/com/shovelant/shovelant/MainActivity.kt
+++ shovelant/app/src/main/java/com/shovelant/shovelant/MainActivity.kt
@@ -2,15 +2,27 @@ package com.shovelant.shovelant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.shovelant.shovelant.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+    lateinit var title :TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = findViewById(R.id.titlesplash)
+
+        //reenable if want to get rid of notification bar/navbar (can also do individually), but have to deal with setting minAPI to 27 to draw below screen cutouts (holepunch)
         //hideSystemBars()
+
     }
 
     private fun hideSystemBars() {
@@ -21,5 +33,11 @@ class MainActivity : AppCompatActivity() {
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         // Hide both the status bar and the navigation bar
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    fun exampleOnClick(view: View) {
+        count++
+        System.out.println(count)
+        title.text = "shovel ant: " + count
     }
 }
